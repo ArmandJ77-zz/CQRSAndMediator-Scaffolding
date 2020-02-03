0e809e04281a0c73297ca8ea7723797257d26226
armand.jordaan@leadhome.co.za <armand.jordaan@leadhome.co.za>
2020-02-03 06:20:18 +0200

    Refactor DomainSettings

---
+++ Models/DomainSettingsModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using CQRSAndMediator.Scaffolding.Enums;
+using CQRSAndMediator.Scaffolding.Infrastructure;
+using CQRSAndMediator.Scaffolding.Resolver;
+using Microsoft.Build.Construction;
+
+namespace CQRSAndMediator.Scaffolding.Models
+{
+    public sealed class DomainSettingsModel
+    {
+        public string Concern { get; }
+        public string Operation { get; }
+        public PatternDirectoryType PatternType { get; }
+        public PatternFileType PatternFileType { get; }
+        public string ClassName { get; }
+        public string ProjectName { get; }
+        public string DomainName { get; }
+        public string DomainAbsolutePath { get; }
+        public DomainSettingsModel(string concern, string operation, PatternDirectoryType patternType)
+        {
+            var solutionFile = Directory.GetFiles(@"C:\Sources\MyGithub\CQRSAndMediator-Microservice", "*.sln").FirstOrDefault();
+
+            var solutionInfo = SolutionFile.Parse(solutionFile);
+
+            var projectList = solutionInfo.ProjectsInOrder;
+
+            foreach (var proj in projectList)
+            {
+                if (!proj.ProjectName.Contains(".Logic")
+                    && !proj.ProjectName.Contains(".Domain"))
+                    continue;
+
+                Concern = concern;
+                Operation = operation;
+                PatternType = patternType;
+
+                ProjectName = proj.RelativePath;
+                DomainName = proj.ProjectName;
+                DomainAbsolutePath = ResolveDomainAbsolutePath(proj.AbsolutePath);
+                PatternFileType = PatternFileNameResolver.Resolve(patternType);
+                ClassName = $"{concern}{operation}{PatternFileNameResolver.Resolve(patternType)}";
+            }
+
+            Log.Info($"ProjectName: {ProjectName}");
+            Log.Info($"DomainName: {DomainName}");
+            Log.Info(DomainAbsolutePath);
+        }
+        private static string ResolveDomainAbsolutePath(string absolutePath)
+        {
+            var lastIndexOf = absolutePath.LastIndexOf("\\", StringComparison.Ordinal);
+            var domainAbsolutePath = absolutePath.Substring(0, lastIndexOf);
+            return domainAbsolutePath;
+        }
+    }
+}
